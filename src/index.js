06ba380f1ff0bd3ac74cafaa576ac889c6e6673a
Nikita <ezin.nik@yandex.ru>
2023-03-21 09:59:38 +0700

    Validation

--- src/index.js
+++ src/index.js
@@ -124,3 +124,55 @@ const createCard = (link, name) => {
   };
 
   popupAddForm.addEventListener('submit', handleItemFormSubmit);
+
+
+const popupForm = document.querySelector('.popup__form');
+const formInput = popupForm.querySelector('.popup__input');
+const formError = popupForm.querySelector(`.${formInput.id}-error`);
+
+
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_activ');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('popup__input-error_activ');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path'); // подключаем path к конфигу вебпак
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин 
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -20,20 +20,34 @@ module.exports = {
   },
   module: {
     rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
-      {
-        // регулярное выражение, которое ищет все js файлы
-        test: /\.js$/,
+      {test: /\.js$/, // регулярное выражение, которое ищет все js файлы
         // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
         // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
-      }]
+      },
+      {test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/, // регулярное выражение, которое ищет все файлы с такими расширениями
+        type: 'asset/resource'
+      },
+      {test: /\.css$/, // применять это правило только к CSS-файлам
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html' // путь к файлу index.html
     }),
     new CleanWebpackPlugin(), // использовали плагин
+    new MiniCssExtractPlugin(), // подключение плагина для объединения файлов
   ]
 };
